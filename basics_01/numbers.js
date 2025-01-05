4c94e7f3178dac61f59f073d4ad8247cb0d06a55
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-05 10:15:22 +0000

    number parsing

---
+++ basics_01/numbers.js
@@ -0,0 +1,42 @@
+
+
+function changeType(num){
+
+    console.log('the og number is ' + num);
+    console.log(typeof(num));
+    let number = parseInt(num);
+    console.log('the parsed number is :' + number)
+    console.log(typeof(number))
+}
+
+// let val = changeType('67')
+// let val1 = changeType('67and')
+// let val2= changeType(67.98)
+
+
+
+// console.log(parseInt('42'));
+// console.log(parseInt('42and'));
+// console.log(parseInt('aaa42'));
+// console.log(parseInt('42.98'));
+
+
+
+function changeTheType(num){
+
+    console.log('the og number is ' + num);
+    console.log(typeof(num));
+    let number = parseFloat(num);
+    console.log('the parsed number is :' + number)
+    console.log(typeof(number))
+}
+
+let value = changeTheType('67')
+let value1 = changeTheType('67and')
+let value2= changeTheType(67.98)
+
+
+// console.log(parseFloat('42'));
+// console.log(parseFloat('42and'));
+// console.log(parseFloat('aaa42'));
+// console.log(parseFloat('42.98'));
