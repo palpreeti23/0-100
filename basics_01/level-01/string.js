db128a06a1fb98d51d73787643382f3bb32848f3
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-07 12:47:52 +0000

    assignment in str

--- basics_01/level-01/string.js
+++ basics_01/level-01/string.js
@@ -58,4 +58,30 @@ function msg4(str){
     console.log('the string in lower case is :'+ str.toLowerCase())
 }
 
-let string = msg4('hIi ThiS is PreEti..pReeti paL');
+// let string = msg4('hIi ThiS is PreEti..pReeti paL');
+
+
+
+
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+    let string = '';
+    for(let i=str.length-1; i>=0;i--){
+         string = string+str[i];
+    }
+    if(string === str){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+  
+  let val = isPalindrome('madam')
+console.log(val)
+
+
