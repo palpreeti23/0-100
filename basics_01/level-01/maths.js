fc2857daefe843fec813dbe1d203f674936da4a4
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-07 14:21:01 +0000

    math functions

---
+++ basics_01/level-01/maths.js
@@ -0,0 +1,23 @@
+let num = Math.random()+1;
+console.log(num);
+
+let floorVal = Math.floor(3.4)
+console.log(floorVal);
+
+let ceilVal = Math.ceil(2.3)
+console.log(ceilVal);
+
+let sqrtVal = Math.sqrt(2)
+console.log(sqrtVal);
+
+let roundVal = Math.round(23.568);
+console.log(roundVal);
+
+let maxVal = Math.max(23,8,98,45,189);
+console.log(maxVal)
+
+let minVal = Math.min(23,8,98,45,189);
+console.log(minVal)
+
+let powerVal = Math.pow(4,2);
+console.log(powerVal)
