7fec7ae71f8cc8324ceab86d702c525e8b21ce0a
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-15 15:40:28 +0000

    removing extra spaces from the file

--- basics_02/practice.js
+++ basics_02/practice.js
@@ -66,6 +66,25 @@
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
 
+// let fs = require('fs');
+
+// // async task
+// function readingFile(){
+
+//     fs.readFile('a.txt', 'utf-8', function(err, data){
+//         console.log('the countent of the file');
+//         console.log(data);
+//         data = data + "im from jairamnagar";
+//         fs.writeFile('a.txt', data, function(err){
+//             console.log('done')
+//             console.log('data after writing :', data)
+//         })
+//     })
+// }
+
+// readingFile()
+
+
 let fs = require('fs');
 
 // async task
@@ -74,12 +93,11 @@ function readingFile(){
     fs.readFile('a.txt', 'utf-8', function(err, data){
         console.log('the countent of the file');
         console.log(data);
-        data = data + "im from jairamnagar";
-        fs.writeFile('a.txt', data, function(err){
-            console.log('done')
-            console.log('data after writing :', data)
+        let update_txt = data.replaceAll(/\s+/g, ' ');
+        fs.writeFile('a.txt', update_txt, function(err){
+            console.log(update_txt)
         })
-    })
+    })  
 }
 
 readingFile()
