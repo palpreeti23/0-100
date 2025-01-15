e43ff49d7a705c3ce422fde16b26502d041a7385
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-15 16:21:44 +0000

    clock counter

--- basics_02/practice.js
+++ basics_02/practice.js
@@ -85,19 +85,58 @@
 // readingFile()
 
 
-let fs = require('fs');
 
-// async task
-function readingFile(){
+// ## File cleaner
+// Read a file, remove all the extra spaces and write it back to the same file.
 
-    fs.readFile('a.txt', 'utf-8', function(err, data){
-        console.log('the countent of the file');
-        console.log(data);
-        let update_txt = data.replaceAll(/\s+/g, ' ');
-        fs.writeFile('a.txt', update_txt, function(err){
-            console.log(update_txt)
-        })
-    })  
+// let fs = require('fs');
+
+// // async task
+// function readingFile(){
+
+//     fs.readFile('a.txt', 'utf-8', function(err, data){
+//         console.log('the countent of the file');
+//         console.log(data);
+//         let update_txt = data.replaceAll(/\s+/g, ' ');
+//         fs.writeFile('a.txt', update_txt, function(err){
+//             console.log(update_txt)
+//         })
+//     })  
+// }
+
+// readingFile()
+
+
+
+// Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
+// clock that shows you the current machine time?
+
+// Can you make it so that it updates every second, and shows time in the following formats - 
+
+//  - HH:MM::SS (Eg. 13:45:23)
+
+//  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+let counterDiv = document.querySelector('.counter');
+let counterDiv2 = document.querySelector('.counter2');
+function currTime(){
+    
+    let now = new Date();
+    let hour = String(now.getHours());
+    let minute = String(now.getMinutes());
+    let sec = String(now.getSeconds());
+
+// for HH:MM:SS 
+    let time = `${hour}:${minute}:${sec}`;
+    counterDiv.textContent = `${time}`
+
+// for second formate
+
+      let hour12 = ((now.getHours()+ 11)%12) + 1;
+      let amPm = now.getHours() >= 12 ? 'PM' : 'AM'
+      counterDiv2.textContent = `${hour12}:${minute}:${sec} ${amPm}`
+    
+    
 }
 
-readingFile()
+setInterval(currTime,1000);
+currTime();
