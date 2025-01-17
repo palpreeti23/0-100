d5762fee66183eaacced640c4fe527667515cb71
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-17 17:07:54 +0000

    promise chain

--- basics_02/practice.js
+++ basics_02/practice.js
@@ -182,6 +182,47 @@
 // * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
 // * Return a promise.all which return the time in milliseconds it takes to complete the entire operation.
 
+// function wait1(t1) {
+//     return new Promise(function(resolve){
+//         setTimeout(() => {
+//             resolve('')
+//         }, t1*1000);
+//     })
+// }
+
+// function wait2(t2) {
+//  return new Promise(function (resolve) {
+//    setTimeout(() => {
+//      resolve("");
+//    }, t2*1000);
+//  });
+// }
+
+// function wait3(t3) {
+//  return new Promise(function (resolve) {
+//    setTimeout(() => {
+//      resolve("");
+//    }, t3*1000);
+//  });
+// }
+// function calculateTime(t1, t2, t3) {
+//     const startTime = Date.now();
+//     return Promise.all([wait1(t1),wait2(t2),wait3(t3)]).then(()=>{
+//         const endTime = Date.now();
+//         return endTime- startTime;
+//     })
+// }
+// calculateTime(1, 2, 3).then((TotalTime)=>{
+//     console.log(`the time is :${TotalTime}` )
+// })
+
+
+
+// Write 3 different functions that return promises that resolve after t1, t2, and t3 seconds respectively.
+//  * Write a function that sequentially calls all 3 of these functions in order.
+//  * Return a promise chain which return the time in milliseconds it takes to complete the entire operation.
+//  * Compare it with the results from 3-promise-all.js
+
 function wait1(t1) {
     return new Promise(function(resolve){
         setTimeout(() => {
@@ -207,10 +248,11 @@ function wait3(t3) {
 }
 function calculateTime(t1, t2, t3) {
     const startTime = Date.now();
-    return Promise.all([wait1(t1),wait2(t2),wait3(t3)]).then(()=>{
+    return wait1().then(()=>wait2().then(()=>wait3().then(()=>{
         const endTime = Date.now();
-        return endTime- startTime;
-    })
+        return endTime-startTime;
+    })));
+    
 }
 calculateTime(1, 2, 3).then((TotalTime)=>{
     console.log(`the time is :${TotalTime}` )
