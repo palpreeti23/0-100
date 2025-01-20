0dbe7954843da3d7bbb6703b6ce54232391be711
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-20 16:36:23 +0000

    express.js

--- basics_02/http-server/server.js
+++ basics_02/http-server/server.js
@@ -1,12 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000;
+// const express = require('express')
+// const app = express()
+// const port = 3000;
 
-app.get('/', function(req,res){
+// app.get('/', function(req,res){
 
-    res.send('hello word')
+//     res.send('hello word')
+// })
+
+// app.listen(port);
+
+
+const express = require('express');
+function sumVal(n){
+    let sum = 0;
+    for(let i= 0; i<n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+const app = express();
+app.get('/', function(req, res){
+    const n = req.query.n;
+    const ans = sumVal(n);
+    res.send(ans.toString())
 })
 
-app.listen(port);
-
-
+app.listen(3001)
