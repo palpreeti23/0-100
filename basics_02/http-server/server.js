3246ed4bc11fe19c10ad24887c2bb59d04c57900
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-01-20 16:50:48 +0000

    express server for sum

--- basics_02/http-server/server.js
+++ basics_02/http-server/server.js
@@ -10,20 +10,35 @@
 // app.listen(port);
 
 
-const express = require('express');
-function sumVal(n){
-    let sum = 0;
-    for(let i= 0; i<n; i++){
-        sum += i;
-    }
-    return sum;
-}
+// const express = require('express');
+// function sumVal(n){
+//     let sum = 0;
+//     for(let i= 0; i<n; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
 
+// const app = express();
+// app.get('/', function(req, res){
+//     const n = req.query.n;
+//     const ans = sumVal(n);
+//     res.send(ans.toString())
+// })
+
+// app.listen(3001);
+
+
+const express = require('express');
+function sumNum(a, b){
+    return a + b;
+}
 const app = express();
 app.get('/', function(req, res){
-    const n = req.query.n;
-    const ans = sumVal(n);
-    res.send(ans.toString())
-})
-
-app.listen(3001)
+        const a = parseInt(req.query.a);
+        const b = parseInt(req.query.b);
+        const ans = sumNum(a, b);
+        res.send(ans.toString())
+    })
+    
+    app.listen(3002);
